39f23cfac946a4c0106b3195de52d1319e8edf3d
ArianaMendonza03 <mendozaariana954@gmail.com>
2021-08-15 22:42:27 +1200

    Algoritmo de Ordenamiento

---
+++ cmake-build-debug/CMakeFiles/Ordenamiento.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Ordenamiento.dir/main.cpp.obj"
+  "Ordenamiento.exe"
+  "Ordenamiento.exe.manifest"
+  "Ordenamiento.pdb"
+  "libOrdenamiento.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Ordenamiento.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
